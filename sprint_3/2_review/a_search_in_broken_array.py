d5cd55e3640e3fb223a85ac6dc3bccce70c5dbb7
Yan-Zhelanov <yzhelanov@gmail.com>
2021-11-02 20:30:04 +0300

    Sprint 3 — Review B (Complete)

--- sprint_3/2_review/a_search_in_broken_array.py
+++ sprint_3/2_review/a_search_in_broken_array.py
@@ -1,19 +1,38 @@
+# if (
+#     nums[middle] < target and min(nums[middle+1:]) <= target and max(nums[middle+1:]) >= target
+#     or nums[middle] >= target and min(nums[:middle]) >= target and max(nums[:middle]) <= target
+# ):
+
+
+# 0 1 2 3 44 55 73 99 -33
+# 44 - middle
+# 0 < 44 --> min [:middle] 0 <= 0 and max [:middle] 3 >= 0 --> right = middle
+
+# 99 > 44 --> min [middle+1:] -33 <= 99 and max [middle+1:] 99 >= 99
+# 73 > 44 --> min [middle+1:] -33 <= 73 and max 99 >= 73
+# 73 > 73 -->
+
 def broken_search(nums, target):
     middle = len(nums) // 2
     left = 0
-    right = len(nums)
+    right = len(nums) - 1
     while left < right:
         if nums[middle] == target:
             return middle
+        if nums[left] == target:
+            return left
+        if nums[right] == target:
+            return right
         if left == middle or right == middle:
             break
-        if (
-            nums[middle] < target and min(nums[middle+1:]) <= target and max(nums[middle+1:]) >= target
-            or nums[middle] > target and min(nums[:middle]) >= target and max(nums[:middle]) <= target
-        ):
-            left = middle
+        if nums[left] <= target <= nums[right]:
+            if nums[middle] < target:
+                left = middle
+            else:
+                right = middle
         else:
-            right = middle
+            left += 1
+            right -= 1
         middle = (left + right) // 2
     return -1
 
@@ -40,9 +59,9 @@ def test_broken_search():
     result = broken_search([0, 1, 2, 3, 44, 55, 73, 99, -33], -33)
     assert result == 8, f'Wrong answer: {result}'
     result = broken_search([0, 1, 2, 3, 44, 55, 73, 99, -33], -99)
-    assert result == 7, f'Wrong answer: {result}'
+    assert result == -1, f'Wrong answer: {result}'
     print('Все тесты пройдены!')
 
 
-if __name__ == '__main__':
-    test_broken_search()
+# if __name__ == '__main__':
+#     test_broken_search()
--- sprint_3/2_review/b_effective_quick_search.py
+++ sprint_3/2_review/b_effective_quick_search.py
@@ -5,17 +5,19 @@ def quick_sort(array):
     def _quick_sort(array, border_left, border_right):
         if border_left >= border_right:
             return array
-        pivot = randint(border_left, border_right)
-        left = border_left
-        right = border_right
-        while left < right:
-            while left < border_right and array[left] < array[pivot]:
+        pivot = array[randint(border_left, border_right)]
+        left, right = border_left, border_right
+        while left <= right:
+            while array[left] < pivot:
                 left += 1
-            while right > border_left and array[right] > array[pivot]:
+            while array[right] > pivot:
                 right -= 1
-            array[left], array[right] = array[right], array[left]
-        _quick_sort(array, left, border_right)
+            if left <= right:
+                array[left], array[right] = array[right], array[left]
+                left += 1
+                right -= 1
         _quick_sort(array, border_left, right)
+        _quick_sort(array, left, border_right)
         return array
 
     return _quick_sort(array, 0, len(array) - 1)
@@ -27,13 +29,27 @@ def preparation_data(array):
         array[index] = [
             -int(array[index][1]), int(array[index][2]), array[index][0]
         ]
+        index += 1
     return array
 
 
+def get_winners_name(winners, index_name=2):
+    return '\n'.join(winner[index_name] for winner in winners)
+
+
 def test_quick_sort():
-    array = [10, 2, 3, 11, 5, 1]
-    result = quick_sort(array)
+    result = quick_sort([10, 2, 3, 11, 5, 1])
     assert result == [1, 2, 3, 5, 10, 11], f'Wrong answer: {result}'
+    result = quick_sort([0, 2, 3, 5, 5, 10, 13, -1])
+    assert result == [-1, 0, 2, 3, 5, 5, 10, 13], f'Wrong answer: {result}'
+    result = quick_sort([99, 10, -10, 30])
+    assert result == [-10, 10, 30, 99], f'Wrong answer: {result}'
+    result = quick_sort([33, 55, -1, -2, -3, 64])
+    assert result == [-3, -2, -1, 33, 55, 64], f'Wrong answer: {result}'
+    print(f'{__name__}: Все тесты пройдены!')
+
+
+def test_get_winners():
     array = preparation_data([
         ['alla', '4', '100'],
         ['gena', '6', '1000'],
@@ -41,12 +57,14 @@ def test_quick_sort():
         ['rita', '2', '90'],
         ['timofey', '4', '80'],
     ])
-    result = quick_sort(array)
+    array_sort = quick_sort(array)
+    result = get_winners_name(array_sort)
     assert result == 'gena\ntimofey\nalla\ngosha\nrita', (
         f'Wrong answer: {result}'
     )
-    print('Все тесты пройдены!')
+    print(f'{__name__}: Все тесты пройдены!')
 
 
 if __name__ == '__main__':
     test_quick_sort()
+    test_get_winners()
