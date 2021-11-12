117d0eb73e1de1a39c0480064430a26c1d3a5258
Yan-Zhelanov <yzhelanov@gmail.com>
2021-11-12 16:09:02 +0300

    Sprint 4 —Issue K (Timelimit)

--- sprint_4/1_issues/k_nearest_station.py
+++ sprint_4/1_issues/k_nearest_station.py
@@ -2,15 +2,21 @@ from math import sqrt
 
 
 def determine_best_exit_metro(metro_stations, bus_stations):
-    bests = {}
-    for index, metro in enumerate(metro_stations):
+    size = len(metro_stations) + 1
+    bests = [0] * size
+    index = 1
+    while index < size:
+        location_x = int(metro_stations[index-1][0])
+        location_y = int(metro_stations[index-1][1])
         for station in bus_stations:
             distance = sqrt(
-                (metro[0] - station[0])**2 + (metro[1] - station[1])**2
+                (location_x - int(station[0]))**2
+                + (location_y - int(station[1]))**2
             )
             if distance <= 20:
-                bests[index+1] = bests.get(index+1, 0) + 1
-    return list(bests.keys())[list(bests.values()).index(max(bests.values()))]
+                bests[index] = bests[index] + 1
+        index = index + 1
+    return bests.index(max(bests))
 
 
 def test_determine_best_exit_metro():
@@ -40,7 +46,7 @@ def test_determine_best_exit_metro():
 if __name__ == '__main__':
     # test_determine_best_exit_metro()
     count = int(input())
-    metro_stations = [list(map(int, input().split())) for _ in range(count)]
+    metro_stations = [input().split() for _ in range(count)]
     count = int(input())
-    bus_stations = [list(map(int, input().split())) for _ in range(count)]
+    bus_stations = [input().split() for _ in range(count)]
     print(determine_best_exit_metro(metro_stations, bus_stations))
