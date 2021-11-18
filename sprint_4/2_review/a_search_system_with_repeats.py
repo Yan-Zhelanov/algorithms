449d29124de5c905d7b7860ff8205d398b1b6edd
Yan-Zhelanov <yzhelanov@gmail.com>
2021-11-18 22:12:16 +0300

    Sprint 4 — Review A (Timelimit)

--- sprint_4/2_review/a_search_system_with_repeats.py
+++ sprint_4/2_review/a_search_system_with_repeats.py
@@ -1,7 +1,8 @@
 def search(docs, requests):
     docs_words = {}
     repeats = {}
-    for index in range(len(docs)):
+    index = 0
+    while index < len(docs):
         if docs[index] in docs[:index]:
             previous = docs[:index].index(docs[index])
             repeats[previous] = repeats.get(previous, []) + [index]
@@ -10,32 +11,30 @@ def search(docs, requests):
         for word in docs[index].split():
             docs_words[word] = docs_words.get(word, {})
             docs_words[word][index] = docs_words[word].get(index, 0) + 1
-    result = []
-    for request_index in range(len(requests)):
-        result.append({index: 0 for index in range(len(docs))})
-        for word in set(requests[request_index].split()):
-            if word in docs_words:
-                for index, count in docs_words[word].items():
-                    result[request_index][index] += count
-    result = [
-        [
-            item[0]+1
-            if item[0] not in repeats
-            else ' '.join(
-                str(index) for index in (
-                    [item[0]+1] + [index+1 for index in repeats[item[0]]]
+        index += 1
+    scores = []
+    for index in range(len(requests)):
+        scores.append({})
+        for word in set(requests[index].split()):
+            if word not in docs_words:
+                continue
+            for doc_index, count in docs_words[word].items():
+                scores[index][doc_index] = (
+                    scores[index].get(doc_index, 0) + count
                 )
-            )
-            for item in sorted(
-                result[index].items(), key=lambda item: (-item[1], item[0])
-            ) if item[1] > 0
-        ] for index in range(len(result))
-    ]
+    result = {}
+    for index in range(len(scores)):
+        for item in sorted(
+            scores[index].items(), key=lambda item: (-item[1], item[0])
+        ):
+            if item[1] <= 0:
+                continue
+            result[index] = result.get(index, []) + [item[0]+1]
+            if item[0] in repeats:
+                result[index] += [index+1 for index in repeats[item[0]]]
     return '\n'.join(
-        ' '.join(str(element[index]) for index in range(5))
-        if len(element) >= 5
-        else ' '.join(str(element[index]) for index in range(len(element)))
-        for element in result
+        ' '.join(str(index) for index in element[:5])
+        for element in result.values() if len(element) > 0
     )
 
 
@@ -64,19 +63,17 @@ def test_search():
     assert result == '4 5 1 2 3\n1 2 3 4 5\n1 2 3 4 5', (
         f'Wrong answer: {result}'
     )
-    result = search(
-        [
-            'buy flat in moscow', 'rent flat in moscow', 'sell flat in moscow',
-            'want flat in moscow like crazy',
-            'clean flat in moscow on weekends', 'renovate flat in moscow',
-        ],
-        [
-            'flat in moscow for crazy weekends', 'flat', 'flat in',
-        ],
-    )
-    assert result == '4 5 1 2 3\n1 2 3 4 5\n1 2 3 4 5', (
-        f'Wrong answer: {result}'
-    )
+    # result = search(
+    #     [
+    #         'one two', 'two two', 'one two', 'two two',
+    #     ],
+    #     [
+    #         'three', 'two', 'two two', 'one', 'one two', 'one two two'
+    #     ],
+    # )
+    # assert result == '2 4 1 3\n2 4 1 3\n1 3\n1 2 3 4\n1 2 3 4', (
+    #     f'Wrong answer: {result}'
+    # )
     result = search(
         [
             'i love ice-cream', 'i love coffee',
@@ -86,21 +83,11 @@ def test_search():
         ],
     )
     assert result == '1 2', f'Wrong answer: {result}'
-    result = search(
-        [
-            'i am copy paste', 'i am copy paste', 'i am copy paste',
-            'i am copy paste', 'i am copy paste', 'i am copy paste',
-        ],
-        [
-            'copy paste',
-        ],
-    )
-    assert result == '1 2 3 4 5 6', f'Wrong answer: {result}'
     print('Все тесты пройдены!')
 
 
 if __name__ == '__main__':
-    # test_search()
+    test_search()
     count = int(input())
     docs = [input() for _ in range(count)]
     count = int(input())
