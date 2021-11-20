74b1ecf3b7017f15dd8fde778033e33b9f716c9f
Yan-Zhelanov <yzhelanov@gmail.com>
2021-11-20 15:37:54 +0300

    Sprint 4 — Review B (Itaration 2)

--- sprint_4/2_review/b_hash_schedule.py
+++ sprint_4/2_review/b_hash_schedule.py
@@ -1,23 +1,25 @@
-# 58681678
+# 58743014
 
 """
     --- Принцип работы ---
-За основу возьмём массив, пустые ячейки будем отмечать, как None, в остальных
-ячейках будем хранить кортежи, где на первом месте будет лежать ключ, на втором
-значение. Так как мы заранее знаем количество запросов, подберём оптимальный
-модуль, длинна массива будет равна этому модулю. Для разрешения коллизий будем
-использовать метод линейной открытой адресации, поэтому для поиска по ключу
-будем хешировать ключ и двигаться по массиву, пока не встретим None, то-есть
-такого ключа ещё нет в массиве или пока не встретим кортеж, с точно таким же
-ключём на первом месте. Поиск по ключу это основная операция, на которой будут
-основываться все остальные действия: добавление, получение и удаление. Для
-добавления элемента или его перезаписи достаточно запустить поиск и
-перезаписать ячейку. Тоже самое с получением, только перезаписывать ячейку мы
-не будем, а если в ячейке лежит None, то-есть такого ключа ещё не было
-добавленно в хеш-таблицу, как и требуется в задании мы будем возвращать строку
-с None. Удаление работает, как эти два метода объединённые вместе, только
-перезаписывать ячейку мы всегда будем None, то-есть будем удалять из неё ключ
-и значение. В случае, если кортежа в ячейке не было, вернём None.
+За основу возьмём массив, пустые ячейки будем отмечать, как None, удалённые
+ячейки будем отмечать как 'deleted' в остальных ячейках будем хранить кортежи,
+где на первом месте будет лежать ключ, на втором значение. Так как мы заранее
+знаем количество запросов, подберём оптимальный модуль, длинна массива будет
+равна этому модулю. Для разрешения коллизий будем использовать метод линейной
+открытой адресации, поэтому для поиска по ключу будем хешировать ключ и
+двигаться по массиву, пока не встретим None или, если мы используем поиск для
+вставки, пока не встретим удалённую ячейку, то-есть такого ключа ещё нет в
+массиве, иначе пока не встретим кортеж, с точно таким же ключём на первом
+месте. Поиск по ключу это основная операция, на которой будут основываться все
+остальные действия: добавление, получение и удаление. Для добавления элемента
+или его перезаписи достаточно запустить поиск и перезаписать ячейку. Тоже самое
+с получением, только перезаписывать ячейку мы не будем, а если в ячейке лежит
+None, то-есть такого ключа ещё не было добавленно в хеш-таблицу, как и
+требуется в задании мы будем возвращать строку с None. Удаление работает,
+как эти два метода объединённые вместе, только перезаписывать ячейку мы всегда
+будем как удалённую, чтобы облегчить поиски в будущем. В случае, если кортежа в
+ячейке не было, вернём None.
 
     --- Доказательство корректности ---
 Из описания следует, что все операции основываются на поиске, поэтому если
@@ -25,9 +27,13 @@
 Модуль, то-есть размер массива у нас не меняется, он всегда простое нечётное
 число, поэтому для того, чтобы разрешать коллизии я выбрал двойку. Ведь даже
 если все элементы будут захешированны одним ключём, поиск пройдётся по каждой
-ячейке массива, пока не найдёт пустую, так как у двойки и простого нечётного
-числа нет общих делителей, а массив превышает размеры запросов. Таким образом
-поиск всегда будет возвращать, либо ячейку с нужным ключём, либо пустую ячейку.
+ячейке массива, пока не пройдёт каждую или не найдёт пустую, так как у двойки и
+простого нечётного числа нет общих делителей, то поиск обязательно проверит
+каждый элемент массива. Таким образом поиск всегда будет возвращать, либо
+ячейку с нужным ключём, либо пустую ячейку, либо удалённую в случае поиска для
+вставки. Если поиск пройдётся по всему массиву, но не найдёт нужной ячейки,
+поиск вернёт None, в случае, если поиск выполнялся для вставки, программа
+предупредит, что было переполнение.
 
     --- Временная сложность ---
 Поиск в худшем случае, когда все элементы будут захешированны одним значением,
@@ -46,6 +52,7 @@ O(n*2) = O(n)
 
 class HashMap:
     MODULE = 1000003
+    DELETED = 'deleted'
 
     def __init__(self):
         self.items = [None] * self.MODULE
@@ -62,27 +69,41 @@ class HashMap:
             index += 1
         return (result + int(key[index])) % module
 
-    def search(self, key):
+    def search(self, key, put=False):
         hashed_key = self.get_hash(key)
-        while (
-            self.items[hashed_key] is not None
-            and self.items[hashed_key][0] != key
-        ):
+        index = 0
+        item = self.items[hashed_key]
+        while not (
+            item is None or put and item == self.DELETED
+            or item != self.DELETED and item[0] == key
+        ) and index < self.MODULE:
             hashed_key = (hashed_key + 2) % self.MODULE
-        return hashed_key
+            item = self.items[hashed_key]
+            index += 1
+        return hashed_key if index < self.MODULE else None
 
     def put(self, key, value):
-        self.items[self.search(key)] = (key, value)
+        hashed_key = self.search(key, True)
+        if hashed_key is None:
+            raise IndexError('Overflow')
+        self.items[hashed_key] = (key, value)
 
     def get(self, key):
-        item = self.items[self.search(key)]
-        return 'None' if item is None else item[1]
+        hashed_key = self.search(key)
+        if hashed_key is None:
+            return None
+        item = self.items[hashed_key]
+        return None if item is None else item[1]
 
     def delete(self, key):
         hashed_key = self.search(key)
+        if hashed_key is None:
+            return None
         item = self.items[hashed_key]
-        self.items[hashed_key] = None
-        return 'None' if item is None else item[1]
+        if item is None:
+            return None
+        self.items[hashed_key] = self.DELETED
+        return item[1]
 
 
 def run_command(instance, name, key, value=None):
@@ -99,10 +120,11 @@ def run_commands(instance, commands):
         if len(command) == 2:
             result.append(run_command(instance, command[0], command[1]))
             continue
-        result.append(
+        try:
             run_command(instance, command[0], command[1], command[2])
-        )
-    return '\n'.join(str(answer) for answer in result if answer is not None)
+        except IndexError as error:
+            result.append(error)
+    return '\n'.join(str(answer) for answer in result)
 
 
 def test_run_commands():
@@ -113,9 +135,17 @@ def test_run_commands():
     assert result == 'None\n10\n4\n4\nNone\n5\nNone', f'Wrong answer: {result}'
     result = run_commands(HashMap(), [
         'delete 2', 'get 1', 'put 34 4', 'get 1', 'delete 33', 'delete 34',
-        'put 1 1', 'put 10000019 2', 'get 1', 'get 10000019'
+        'put 1 1', 'put 1000003 2', 'get 1', 'get 1000003'
     ])
-    assert result == 'None\nNone\nNone\nNone\n4\n1\n2', f'Wrong answer: {result}'
+    assert result == 'None\nNone\nNone\nNone\n4\n1\n2', (
+        f'Wrong answer: {result}'
+    )
+    result = run_commands(HashMap(), [
+        'put 1 1', 'put 2 2', 'put 3 3', 'delete 2', 'get 3', 'get 1', 'get 2'
+    ])
+    assert result == '2\n3\n1\nNone', (
+        f'Wrong answer: {result}'
+    )
     print('Все тесты пройдены!')
 
 
@@ -129,7 +159,8 @@ if __name__ == '__main__':
         if len(command) == 2:
             result.append(run_command(hash_map, command[0], command[1]))
             continue
-        result.append(
+        try:
             run_command(hash_map, command[0], command[1], command[2])
-        )
-    print('\n'.join(answer for answer in result if answer is not None))
+        except IndexError as error:
+            result.append(error)
+    print('\n'.join(str(answer) for answer in result))
