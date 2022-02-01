ba331c948b44a873dff667cf6010e751a066a250
Yan-Zhelanov <yzhelanov@gmail.com>
2022-02-01 23:30:51 +0300

    Sprint 2 — Issue L (Complete)

--- sprint_2/1_issues/l_fibonacсi_modulo.py
+++ sprint_2/1_issues/l_fibonacсi_modulo.py
@@ -1,35 +1,40 @@
-from math import fsum, pow
+# @profile
+def get_fibonacci_last_numbers(number, count_last):
+    first = 0
+    second = 1
+    result = 1
+    index = 0
+    module = 10 ** count_last
+    while index < number:
+        result = (first + second) % module
+        first = second
+        second = result
+        index += 1
+    return result
 
 
-def get_fibonacci_last_nums(num, count_last):
-    num += 1
-    left = pow(((1 + pow(5, 0.5)) / 2), num)
-    right = -pow(((1 - pow(5, 0.5)) / 2), num)
-    result = (
-        fsum([left, right])
-        / pow(5, 0.5)
-    ) % pow(10, count_last)
-    return f'{result:.0f}'
-
-
-def test_get_fibonacci_last_nums():
-    result = get_fibonacci_last_nums(3, 1)
-    assert result == '3', f'Wrong answer: {result}'
-    result = get_fibonacci_last_nums(10, 1)
-    assert result == '9', f'Wrong answer: {result}'
-    result = get_fibonacci_last_nums(11, 1)
-    assert result == '4', f'Wrong answer: {result}'
-    result = get_fibonacci_last_nums(11, 5)
-    assert result == '144', f'Wrong answer: {result}'
-    result = get_fibonacci_last_nums(21, 5)
-    assert result == '17711', f'Wrong answer: {result}'
-    result = get_fibonacci_last_nums(21, 4)
-    assert result == '7711', f'Wrong answer: {result}'
-    result = get_fibonacci_last_nums(484, 2)
-    assert result == '85', f'Wrong answer: {result}'
-    print('Все тесты пройдены!')
+def test_get_fibonacci_last_numbers():
+    result = get_fibonacci_last_numbers(0, 5)
+    assert result == 1, f'Wrong answer: {result}'
+    result = get_fibonacci_last_numbers(3, 1)
+    assert result == 3, f'Wrong answer: {result}'
+    result = get_fibonacci_last_numbers(10, 1)
+    assert result == 9, f'Wrong answer: {result}'
+    result = get_fibonacci_last_numbers(11, 1)
+    assert result == 4, f'Wrong answer: {result}'
+    result = get_fibonacci_last_numbers(11, 5)
+    assert result == 144, f'Wrong answer: {result}'
+    result = get_fibonacci_last_numbers(21, 5)
+    assert result == 17711, f'Wrong answer: {result}'
+    result = get_fibonacci_last_numbers(21, 4)
+    assert result == 7711, f'Wrong answer: {result}'
+    result = get_fibonacci_last_numbers(484, 2)
+    assert result == 85, f'Wrong answer: {result}'
+    result = get_fibonacci_last_numbers(506277, 6)
+    assert result == 263439, f'Wrong answer: {result}'
+    print('All tests passed!')
 
 
 if __name__ == '__main__':
-    test_get_fibonacci_last_nums()
-    print(get_fibonacci_last_nums(*map(int, input().split())))
+    # test_get_fibonacci_last_numbers()
+    print(get_fibonacci_last_numbers(*map(int, input().split())))
