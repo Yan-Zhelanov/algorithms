0b2334aed399a022b97c4c813f9baf4ee9ff29f9
Yan-Zhelanov <yzhelanov@gmail.com>
2022-02-02 00:04:28 +0300

    Sprint 3 — Issue B (Complete)

--- sprint_3/1_issues/b_combinations.py
+++ sprint_3/1_issues/b_combinations.py
@@ -10,24 +10,30 @@ BUTTONS = {
 }
 
 
-def generate_texts(numbers, count=None, text='', result=''):
-    if count is None:
-        count = len(numbers)
-    if count == 0:
-        result += text + ' '
-    else:
+def generate_texts(numbers):
+    def _generate_sequences(numbers, count=None, text='', result=[]):
+        if count is None:
+            count = len(numbers)
+        if count == 0:
+            result.append(text)
+            return result
         for letter in BUTTONS[numbers[-count]]:
-            result = generate_texts(numbers, count-1, text+letter, result)
-    return result
+            result = _generate_sequences(numbers, count-1, text+letter, result)
+        return result
+
+    return ' '.join(_generate_sequences(numbers))
 
 
 def test_generate_texts():
     result = generate_texts('23')
-    assert result == 'ad ae af bd be bf cd ce cf ', f'Wrong answer: {result}'
-    result = generate_texts('689372357')
-    print('Все тесты пройдены!')
+    assert result == 'ad ae af bd be bf cd ce cf', f'Wrong answer: {result}'
+    result = generate_texts('2')
+    assert result == 'a b c', f'Wrong answer: {result}'
+    result = generate_texts('22')
+    assert result == 'aa ab ac ba bb bc ca cb cc', f'Wrong answer: {result}'
+    print('All tests passed!')
 
 
 if __name__ == '__main__':
-    test_generate_texts()
-    # print(generate_texts(input()))
+    # test_generate_texts()
+    print(generate_texts(input()))
